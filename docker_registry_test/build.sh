587c0b799d9e5645b72aa88f9dffb8701cf1c46e
ynr <ya_onur@hotmail.com>
2023-01-05 20:37:32 +0300

    docker test

--- docker_registry_test/build.sh
+++ docker_registry_test/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+docker rmi docker_registry_test -f
 docker build -t docker_registry_test:latest --network=host .
 #docker push r:latest
--- docker_registry_test/test.py
+++ docker_registry_test/test.py
@@ -1,7 +1,14 @@
 import time
 import os
+import logging
 
+logger = logging.getLogger("test")
+
+i = 0
 while True:
-    print("this is running")
-    print(f"cwd is {os.getcwd()}")
-    time.sleep(5)
+    logger.warning("this is running")
+    logger.warning(f"cwd is {os.getcwd()}")
+    time.sleep(1)
+    i += 1
+    if i == 5:
+        break
