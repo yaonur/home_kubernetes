de7a5e30f6a1b8424162669ca821b0b194959f04
ynr <ya_onur@hotmail.com>
2023-01-06 05:31:33 +0300

    harbor

--- docker_registry_test/build.sh
+++ docker_registry_test/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #docker rmi docker_registry_test -f
 #docker build -t docker_registry_test:latest --network=host .
-docker build -t so2harbor.com/library/docker_registry_test:latest --network=host .
-docker push  so2harbor.com/library/docker_registry_test:latest
+docker build -t so2harbor.com:6000/library/docker_registry_test:latest --network=host .
+docker push  so2harbor.com:6000/library/docker_registry_test:latest
 #docker push r:latest
