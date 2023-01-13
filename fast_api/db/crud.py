7f84c34bcb71a2079bf6330d75eb925211c1deb6
ynr <ya_onur@hotmail.com>
2023-01-13 05:30:38 +0300

    fast api

--- fast_api/db/crud.py
+++ fast_api/db/crud.py
@@ -1,11 +1,11 @@
 from typing import Callable, List, Any
 from sqlalchemy.orm import Session
-from .model import Blog
+from .models import Blog
 from schemas import BlogSchema
 
 
-def get_blog(db: Session, skip: int = 0, limit: int = 100) -> Callable[[], list[Blog]]:
-    return db.query(Blog).offset(skip).limit(limit).all
+def get_blogs(db: Session, skip: int = 0, limit: int = 100) -> List[Blog]:
+    return db.query(Blog).offset(skip).limit(limit).all()
 
 
 def get_blog_by_id(db: Session, blog_id: int) -> Blog:
@@ -13,21 +13,25 @@ def get_blog_by_id(db: Session, blog_id: int) -> Blog:
 
 
 def create_blog(db: Session, blog: BlogSchema) -> Blog:
-    _blog = Blog(title=blog.title,body=blog.body,published=blog.published)
+    _blog = Blog(title=blog.title, body=blog.body, published=blog.published)
     db.add(_blog)
     db.commit()
     db.refresh(_blog)
     return _blog
 
 
-def remove_blog(db: Session, blog_id: int):
+def remove_blog(db: Session, blog_id: int) -> Blog:
     _blog = get_blog_by_id(db=db, blog_id=blog_id)
     db.delete(_blog)
     db.commit()
+    return _blog
 
 
 def update_blog(db: Session, blog_id: int, title: str, body: str, published: bool) -> Blog:
     _blog = get_blog_by_id(db=db, blog_id=blog_id)
+    if not _blog:
+        return _blog
+
     _blog.title = title
     _blog.body = body
     _blog.published = published
--- fast_api/main.py
+++ fast_api/main.py
@@ -1,26 +1,46 @@
 from typing import Optional
-from fastapi import FastAPI, Depends
+
+import starlette.exceptions
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from sqlalchemy.orm import Session
-from db import crud
 from schemas import BlogSchema
-from db import model
+from db import models, crud
 from db.config import engine, get_db
 
-model.Base.metadata.create_all(bind=engine)
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
 
 @app.get('/')
 def index():
-    return {"hy": "ss"}
+    return {"Blogger": "ss"}
 
 
-@app.get('/blog')
-def index(published: bool, limit=10, sort: Optional[str] = None):
-    if published:
-        return {'data': f'{limit} published blogs from the db '}
-    return {'data': f'{limit} unpublished blogs from the db'}
+@app.get('/blogs')
+def get_blogs(db: Session = Depends(get_db)):
+    blogs = crud.get_blogs(db)
+    return blogs
+
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create_blog(blog_req: BlogSchema, db: Session = Depends(get_db)):
+    blog = crud.create_blog(db, blog_req)
+    return blog
+
+
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete_blog(blog_id: int, db: Session = Depends(get_db)):
+    blog = crud.remove_blog(db, blog_id)
+    return 'done'
+
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update_blog(updated_blog: BlogSchema, blog_id: int, db: Session = Depends(get_db)):
+    blog = crud.update_blog(db, blog_id, updated_blog.title, updated_blog.body, updated_blog.published)
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"blog with {blog_id} not found")
+    return 'updated'
 
 
 @app.get('/blog/unbuplished')
@@ -28,18 +48,18 @@ def unpublished():
     return {'data': 'all unpublished blogs'}
 
 
-@app.get('/blog/{blog_id}')
-def blog(blog_id: int):
-    return {"blog": {blog_id}}
+@app.get('/blog/{blog_id}', status_code=status.HTTP_200_OK)
+def get_blog_by_id(response: Response, blog_id: int, db: Session = Depends(get_db)):
+    blog = crud.get_blog_by_id(db, blog_id)
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Blog with the id {blog_id} is not available ")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail': f'Blog with the id {blog_id} is not available'}
+    return {blog}
 
 
 @app.get('/blog/{id}/comments')
 def comments(id):
     # fetch comments with id
     return {'data': {'1', '2'}}
-
-
-@app.post('/blog')
-def create_blog(blog_req: BlogSchema, db: Session = Depends(get_db)):
-    crud.create_blog(db, blog_req)
-    return {'title': blog_req.title, 'body': blog_req.body}
