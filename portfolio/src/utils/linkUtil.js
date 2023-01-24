6673eca4a180f479907636cc5252a64e99eb07b9
ynr <ya_onur@hotmail.com>
2023-01-25 00:10:33 +0300

    portfolio

---
+++ portfolio/src/utils/linkUtil.js
@@ -0,0 +1,16 @@
+import svelteLogo from '../assets/svelte.svg'
+
+export const navbarData = {
+    logo: true,
+    logoSrc: svelteLogo,
+    logoLink: true,
+    linkUrl: '#/',
+    optionalLinkText: 'svelte spa',
+    altText: 'Logo',
+    links: [
+        {url: '#/', displayInNav: true, displayInFooter: true, linkText: 'Home'},
+        {url: '#/About', displayInNav: true, displayInFooter: true, linkText: 'About'},
+        {url: '#/Contact', displayInNav: true, displayInFooter: true, linkText: 'Contact'},
+        {url: '#/Projects', displayInNav: true, displayInFooter: true, linkText: 'Projects'},
+    ]
+}
